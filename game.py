b7838718b8ce7f8b924fe412486f55c01945c312
andrewgalvanarrien <147419694+andrewgalvanarrien@users.noreply.github.com>
2023-10-12 09:43:55 -0400

    ocean+sand

--- game.py
+++ game.py
@@ -1,3 +1,19 @@
 import pygame
+import time
+
+print(f" the quit event is type {pygame.QUIT}")
+pygame.init()
 
 
+screen = pygame.display.set_mode((400,400))
+pygame.display.set_caption("Chomp!")
+screen.fill((0,0,255))
+pygame.draw.rect(screen, (100,25,0), (0, 380, 400, 400))
+pygame.draw
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        print(event.type)
+        if event.type == pygame.QUIT:
+            print("Not today! You will play forever!!!")
