7fbe8448083d941f048ab0ad46dd4a4036a16316
andrewgalvanarrien <147419694+andrewgalvanarrien@users.noreply.github.com>
2023-10-12 09:45:04 -0400

    added a fish

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((400,400))
 pygame.display.set_caption("Chomp!")
 screen.fill((0,0,255))
 pygame.draw.rect(screen, (100,25,0), (0, 380, 400, 400))
-pygame.draw
+pygame.draw.rect(screen, (0,255,0), (200,200, 50,50))
 pygame.display.flip()
 
 while True:
