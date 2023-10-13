8de2406901864f9d3b3ade7af7b9e8de98223543
andrewgalvanarrien <147419694+andrewgalvanarrien@users.noreply.github.com>
2023-10-13 09:44:51 -0400

    Update game.py

--- game.py
+++ game.py
@@ -10,6 +10,8 @@ screen = pygame.display.set_mode((400,400))
 pygame.display.set_caption("Chomp!")
 screen.fill((114,159,255))
 pygame.draw.rect(screen, (100,25,0), (0, 380, 400, 400))
+sand = pygame.image.load("assets/images/sand.png").convert()
+screen.blit(sand, (200,200,64,64))
 pygame.draw.rect(screen, (0,255,0), (200,200, 50,50))
 pygame.display.flip()
 
