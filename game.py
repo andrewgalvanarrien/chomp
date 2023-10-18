6919dea1adc6fb3fde95e57ad4333da13b011450
andrewgalvanarrien <147419694+andrewgalvanarrien@users.noreply.github.com>
2023-10-18 09:45:59 -0400

    first for loop for sand

--- game.py
+++ game.py
@@ -3,10 +3,10 @@ import time
 import sys
 
 # Dimensions
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
+TILE_SIZE = 64  # tiles are squares
+SCREEN_WIDTH = 8 * TILE_SIZE
+SCREEN_HEIGHT = 8 * TILE_SIZE
 SAND_HEIGHT = 20
-TILE_SIZE = 64  #   tiles are squares
 
 # Colors
 WATER_COLOR = (114,159,255)
@@ -19,7 +19,13 @@ pygame.display.set_caption("Chomp!")
 screen.fill(WATER_COLOR)
 pygame.draw.rect(screen, SAND_COLOR, (0, SCREEN_HEIGHT-SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand, (SCREEN_WIDTH/2 - TILE_SIZE/2, SCREEN_HEIGHT/2 - TILE_SIZE/2, TILE_SIZE, TILE_SIZE))
+sand_top = pygame.image.load("assets/images/sand_top.png").convert()
+sand_top.set_colorkey((0,0,0))
+
+# Blit sand tiles across the bottom of the screen
+for i in range(SCREEN_WIDTH // TILE_SIZE):
+    screen.blit(sand, (i * TILE_SIZE, SCREEN_HEIGHT-TILE_SIZE, TILE_SIZE, TILE_SIZE))
+    screen.blit(sand_top, (i * TILE_SIZE, SCREEN_HEIGHT - (2 * TILE_SIZE), TILE_SIZE, TILE_SIZE))
 
 
 while True:
