27bcf2bd7e31046be3932b63f3964de4e2d5d2f9
andrewgalvanarrien <147419694+andrewgalvanarrien@users.noreply.github.com>
2023-10-18 09:25:30 -0400

    Made code cleaner

--- game.py
+++ game.py
@@ -2,18 +2,25 @@ import pygame
 import time
 import sys
 
-print(f" the quit event is type {pygame.QUIT}")
+# Dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+SAND_HEIGHT = 20
+TILE_SIZE = 64  #   tiles are squares
+
+# Colors
+WATER_COLOR = (114,159,255)
+SAND_COLOR = (100,25,0)
+
 pygame.init()
 
-
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
-screen.fill((114,159,255))
-pygame.draw.rect(screen, (100,25,0), (0, 380, 400, 400))
+screen.fill(WATER_COLOR)
+pygame.draw.rect(screen, SAND_COLOR, (0, SCREEN_HEIGHT-SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand, (200,200,64,64))
-pygame.draw.rect(screen, (0,255,0), (200,200, 50,50))
-pygame.display.flip()
+screen.blit(sand, (SCREEN_WIDTH/2 - TILE_SIZE/2, SCREEN_HEIGHT/2 - TILE_SIZE/2, TILE_SIZE, TILE_SIZE))
+
 
 while True:
     for event in pygame.event.get():
@@ -22,3 +29,5 @@ while True:
             print("Thanks for playing")
             pygame.quit()
             sys.exit()
+
+        pygame.display.flip()
