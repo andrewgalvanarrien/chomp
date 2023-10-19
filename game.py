e72513fc7df00f994924ba78b2d06b1dfff96936
andrewgalvanarrien <147419694+andrewgalvanarrien@users.noreply.github.com>
2023-10-19 09:45:52 -0400

    Everything

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import sys
+import fish
 import random
 
 from settings import *
@@ -11,7 +12,6 @@ game_font = pygame.font.Font("assets/fonts/Black_Crayon.ttf", 128)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
 screen.fill(WATER_COLOR)
-pygame.draw.rect(screen, SAND_COLOR, (0, SCREEN_HEIGHT-SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()
 sand_top = pygame.image.load("assets/images/sand_top.png").convert()
 seagrass = pygame.image.load("assets/images/seagrass.png").convert()
@@ -22,29 +22,26 @@ seagrass.set_colorkey((0,0,0))
 star.set_colorkey((0,0,0))
 rock.set_colorkey((0,0,0))
 
-# Blit sand tiles across the bottom of the screen
-for i in range(SCREEN_WIDTH // TILE_SIZE):
-    screen.blit(sand, (i * TILE_SIZE, SCREEN_HEIGHT-TILE_SIZE, TILE_SIZE, TILE_SIZE))
-    screen.blit(sand_top, (i * TILE_SIZE, SCREEN_HEIGHT - (2 * TILE_SIZE), TILE_SIZE, TILE_SIZE))
+my_fish = fish.Fish(200, 200) # create a new fish
+background = screen.copy()
 
-for i in range(4):
-    x = random.randint(0, SCREEN_WIDTH-TILE_SIZE)
-    y = random.randint(SCREEN_HEIGHT - (2 * TILE_SIZE), SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
-    screen.blit(seagrass, (x, y, TILE_SIZE, TILE_SIZE))
+def draw_background():
 
-#for i in range(2):
-   # x = random.randint(0, SCREEN_WIDTH - TILE_SIZE)
-   # y = random.randint(SCREEN_HEIGHT - (2 * TILE_SIZE), SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
-   # screen.blit(star, (x, y, TILE_SIZE, TILE_SIZE))
+    background.fill(WATER_COLOR)
+    # Blit sand tiles across the bottom of the screen
+    for i in range(SCREEN_WIDTH // TILE_SIZE):
+        background.blit(sand, (i * TILE_SIZE, SCREEN_HEIGHT-TILE_SIZE, TILE_SIZE, TILE_SIZE))
+        background.blit(sand_top, (i * TILE_SIZE, SCREEN_HEIGHT - (2 * TILE_SIZE), TILE_SIZE, TILE_SIZE))
 
-#for i in range(2):
-   # x = random.randint(0, SCREEN_WIDTH - TILE_SIZE)
-   # y = random.randint(SCREEN_HEIGHT - (2 * TILE_SIZE), SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
-   # screen.blit(rock, (x, y, TILE_SIZE, TILE_SIZE))
+    for i in range(4):
+        x = random.randint(0, SCREEN_WIDTH-TILE_SIZE)
+        y = random.randint(SCREEN_HEIGHT - (2 * TILE_SIZE), SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
+        background.blit(seagrass, (x, y, TILE_SIZE, TILE_SIZE))
 
-text = game_font.render("Chomp!", True, (255, 69, 0))
-text.get_width()
-screen.blit(text, (SCREEN_WIDTH//2 - text.get_width()//2,SCREEN_HEIGHT//2 - text.get_height()//2))
+    text = game_font.render("Chomp!", True, (255, 69, 0))
+    background.blit(text, (SCREEN_WIDTH//2 - text.get_width()//2,SCREEN_HEIGHT//2 - text.get_height()//2))
+
+draw_background()
 
 while True:
     for event in pygame.event.get():
@@ -54,4 +51,15 @@ while True:
             pygame.quit()
             sys.exit()
 
-        pygame.display.flip()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                my_fish.move_left()
+                print("left arrow pressed")
+            if event.key == pygame.K_RIGHT:
+                my_fish.move_right()
+                print("right arrow pressed")
+
+    # update screen
+    screen.blit(background, (0,0))
+    my_fish.draw(screen)
+    pygame.display.flip()
--- settings.py
+++ settings.py
@@ -7,3 +7,17 @@ SAND_HEIGHT = 20
 # Colors
 WATER_COLOR = (114,159,255)
 SAND_COLOR = (100,25,0)
+
+fish_x = 200
+fish_y = 200
+
+
+#for i in range(2):
+   # x = random.randint(0, SCREEN_WIDTH - TILE_SIZE)
+   # y = random.randint(SCREEN_HEIGHT - (2 * TILE_SIZE), SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
+   # screen.blit(star, (x, y, TILE_SIZE, TILE_SIZE))
+
+#for i in range(2):
+   # x = random.randint(0, SCREEN_WIDTH - TILE_SIZE)
+   # y = random.randint(SCREEN_HEIGHT - (2 * TILE_SIZE), SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
+   # screen.blit(rock, (x, y, TILE_SIZE, TILE_SIZE))
